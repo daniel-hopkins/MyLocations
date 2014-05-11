a0c85300cfcd200c21929db543152953158a6c20
Daniel Hopkins <d@niestudio.com>
2014-05-11 16:57:05 -0700

    Import corelocation, get and NSLog current location

--- MyLocations/CurrentLocationViewController.m
+++ MyLocations/CurrentLocationViewController.m
@@ -13,6 +13,19 @@
 @end
 
 @implementation CurrentLocationViewController
+{
+	CLLocationManager *_locationManager;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+	if ((self = [super initWithCoder:aDecoder]))
+	{
+		_locationManager = [[CLLocationManager alloc] init];
+	}
+	
+	return self;
+}
 
 - (void)viewDidLoad
 {
@@ -28,7 +41,22 @@
 
 - (IBAction)getLocation:(id)sender
 {
+	_locationManager.delegate = self;
+	_locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+	[_locationManager startUpdatingLocation];
+}
+
+#pragma mark - CLLocationManagerDelegate
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+	NSLog(@"didFailWithError %@", error);
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+	CLLocation *newLocation = [locations lastObject];
 	
+	NSLog(@"didUpdateLocation %@", newLocation);
 }
 
 @end
