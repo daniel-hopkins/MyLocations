366a25f80ee76b7c8dd0a247720fb4b7571d3d7a
Daniel Hopkins <d@niestudio.com>
2014-05-22 17:20:27 -0700

    create data model

---
+++ MyLocations/Location.h
@@ -0,0 +1,22 @@
+//
+//  Location.h
+//  MyLocations
+//
+//  Created by Daniel Hopkins on 5/22/14.
+//  Copyright (c) 2014 niestudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Location : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * latitude;
+@property (nonatomic, retain) NSNumber * longitude;
+@property (nonatomic, retain) NSDate * date;
+@property (nonatomic, retain) NSString * locationDescription;
+@property (nonatomic, retain) NSString * category;
+@property (nonatomic, retain) CLPlacemark *placemark;
+
+@end
---
+++ MyLocations/Location.m
@@ -0,0 +1,21 @@
+//
+//  Location.m
+//  MyLocations
+//
+//  Created by Daniel Hopkins on 5/22/14.
+//  Copyright (c) 2014 niestudio. All rights reserved.
+//
+
+#import "Location.h"
+
+
+@implementation Location
+
+@dynamic latitude;
+@dynamic longitude;
+@dynamic date;
+@dynamic locationDescription;
+@dynamic category;
+@dynamic placemark;
+
+@end
