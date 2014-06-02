630cd68401d681c712903ee07cabc41d5e82350d
Daniel Hopkins <d@niestudio.com>
2014-06-02 06:35:48 -0700

    Map WIP

---
+++ MyLocations/MapViewController.h
@@ -0,0 +1,15 @@
+//
+//  MapViewController.h
+//  MyLocations
+//
+//  Created by Daniel Hopkins on 6/1/14.
+//  Copyright (c) 2014 niestudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+
+@end
